ae11303388c3a4733ff8e39491184c2758ad9864
Dave Caffrey <dave.caffrey71@gmail.com>
2020-05-27 16:40:19 +0100

    basic template logic of products page

--- product/models.py
+++ product/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    num_of_sales = models.IntegerField(default=0)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    short_description = models.CharField(max_length=300)
+    long_description = models.CharField(max_length=500)
+    rating = models.DecimalField(max_digits=2, decimal_places=1, default=0)
+
+    def __str__(self):
+        return self.name
+
+
--- product/views.py
+++ product/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
+
+
+def index(request):
+    product_list = Product.objects.all()
+    context = {'products': product_list}
+    return render(request, 'product/index.html',context)
